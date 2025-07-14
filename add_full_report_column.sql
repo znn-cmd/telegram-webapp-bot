bbe589987e5e59939874eb46fc7b92ed6435103b
zaicevn <zaicevn08@gmail.com>
2025-07-14 15:20:17 +0300

    Полная реализация WebApp с макроэкономическими данными и PDF функциональностью

--- add_full_report_column.sql
+++ add_full_report_column.sql
@@ -1,6 +1,6 @@
 -- Добавляем колонку full_report в таблицу user_reports
 ALTER TABLE user_reports 
 ADD COLUMN IF NOT EXISTS full_report JSONB;
-
+ 
 -- Добавляем комментарий к колонке
 COMMENT ON COLUMN user_reports.full_report IS 'Полный отчет в формате JSON с детальным анализом'; 
--- check_table_structure.sql
+++ check_table_structure.sql
@@ -3,7 +3,7 @@ SELECT column_name, data_type, is_nullable
 FROM information_schema.columns 
 WHERE table_name = 'user_reports' 
 ORDER BY ordinal_position;
-
+ 
 -- Проверяем, есть ли таблица вообще
 SELECT table_name 
 FROM information_schema.tables 
